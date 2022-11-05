dffd5c0c568716652b5069f07b6b96ec4da381ff
kataao <n.che@list.ru>
2022-11-05 23:14:23 +0300

    change link to current url

--- test_main_page.py
+++ test_main_page.py
@@ -20,5 +20,5 @@ def test_guest_should_see_login_page(browser):
     main_page = MainPage(browser, link)
     main_page.open()
     main_page.go_to_login_page()
-    login_page = LoginPage(browser, link)
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
